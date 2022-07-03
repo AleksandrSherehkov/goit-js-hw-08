8bae767d0da49c68f2c13a57d0b46576d68df957
Aleksandr Shereshkov <aleksandr.shereshkov@gmail.com>
2022-07-04 00:56:23 +0300

    add dz

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
 
-console.log(galleryItems);
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryRefs = document.querySelector('.gallery');
+
+const createGalleryMarkUp = galleryItems => {
+  const itemMarkup = galleryItems
+    .map(({ original, preview, description }) => {
+      return `
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"   
+    />
+  </a>
+  `;
+    })
+    .join('');
+  galleryRefs.insertAdjacentHTML('beforeend', itemMarkup);
+};
+
+createGalleryMarkUp(galleryItems);
+
+new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
